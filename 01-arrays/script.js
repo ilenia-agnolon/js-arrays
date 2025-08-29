1fe9292b18234c044751ae857e94fab4eaa681b7
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-08-29 18:08:51 +0200

    5.Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -28,6 +28,9 @@ console.log("primo insegnante:", firstTeacher);
 console.log("array aggiornato", teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push('Vanessa');
+console.log(teachers);
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
